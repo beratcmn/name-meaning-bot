ceacc28e574a4e001df6f7b6e19096b958299e97
Berat Çimen <beratcmn@hotmail.com>
2022-10-17 02:45:17 +0200

    Update

--- src/main.py
+++ src/main.py
@@ -31,8 +31,8 @@ def GenerateImage(_name: str, _meaning: str):
 
 
 def main():
-    # with open("data.json", "r", encoding="utf-8") as f:
-    with open("data-news.json", "r", encoding="utf-8") as f:
+    with open("data.json", "r", encoding="utf-8") as f:
+    # with open("data-news.json", "r", encoding="utf-8") as f:
         data = json.load(f)
 
     for entry in data:
